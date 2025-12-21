f2aabed8ddcfa97a09f26f62854bc7c91674b29f
vandana-pathak12 <vandanapathak1212@gmail.com>
2025-12-21 15:46:41 +0530

    do

--- puzzel game/script.js
+++ puzzel game/script.js
@@ -2,4 +2,10 @@ const puzzles = {
     question:"what comes next in the series:2,4,8,16,?",
     options:["18","24","32","20"],
     answer:"32"
+},
+{
+    question:"if all roses are flowers and some flowers fade then:",
+    options:[
+        "all roses fade",
+    ]
 }
