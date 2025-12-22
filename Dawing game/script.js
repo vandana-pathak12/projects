1a0c329f08b9f92fbd019a567f218f776ceaf7dc
vandana-pathak12 <vandanapathak1212@gmail.com>
2025-12-23 00:09:20 +0530

    do

--- Dawing game/script.js
+++ Dawing game/script.js
@@ -1,38 +1,29 @@
-const canvas = document.getElementById("board");
-const ctx = canvas.getContext("2d");
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
-canvas.width = 600;
-canvas.height = 400;
+    let drawing = false;
 
-let drawing = false;
+    canvas.addEventListener("mousedown", () => drawing = true);
+    canvas.addEventListener("mouseup", () => {
+        drawing = false;
+        ctx.beginPath();
+    });
 
+    canvas.addEventListener("mousemove", draw);
 
-canvas.addEventListener("mousedown", () => {
-    drawing = true;
-});
+    function draw(e) {
+        if (!drawing) return;
 
+        ctx.lineWidth = document.getElementById("size").value;
+        ctx.strokeStyle = document.getElementById("color").value;
+        ctx.lineCap = "round";
 
-canvas.addEventListener("mouseup", () => {
-    drawing = false;
-    ctx.beginPath(); 
-});
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.offsetX, e.offsetY);
+    }
 
-canvas.addEventListener("mousemove", draw);
-
-function draw(event) {
-    if (!drawing) return;
-
-    ctx.lineWidth = 2;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = "black";
-
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(event.offsetX, event.offsetY);
-}
-
-
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+    function clearCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
