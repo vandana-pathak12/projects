eb12a6e23960f3705b5a5f214340f2157677d1bc
vandana-pathak12 <vandanapathak1212@gmail.com>
2025-12-23 00:03:37 +0530

    do

---
+++ Dawing game/script.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 400;
+
+let drawing = false;
+
+// Mouse down → start drawing
+canvas.addEventListener("mousedown", () => {
+    drawing = true;
+});
+
+// Mouse up → stop drawing
+canvas.addEventListener("mouseup", () => {
+    drawing = false;
+    ctx.beginPath(); // new line start
+});
+
+// Mouse move → draw
+canvas.addEventListener("mousemove", draw);
+
+function draw(event) {
+    if (!drawing) return;
+
+    ctx.lineWidth = 2;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+// Clear button
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
